b81539abd68ca321ca1a44e7f42a9d414b919748
yoyojacky <yoyojacky@users.noreply.github.com>
2017-05-05 12:22:50 +0800

    update calibrator.sh
    update calibrator.sh

--- calibrator.sh
+++ calibrator.sh
@@ -76,7 +76,6 @@ R1)
   sudo sed -i '/hdmi_group.*/d' /boot/config.txt
   sudo sed -i '/hdmi_mode.*/d' /boot/config.txt
   sudo sed -i '/hdmi_cvt.*/d' /boot/config.txt
-  sudo sed -i '/hdmi_edid_file.*/d' /boot/config.txt
   sudo sed -i '/hdmi_force/a\hdmi_group=2' /boot/config.txt
   sudo sed -i '/hdmi_force/a\hdmi_mode=87' /boot/config.txt
   sudo sed -i '/hdmi_force/a\hdmi_cvt 800 480 60 6 0 0 0' /boot/config.txt
